f04d8ea357a18c906b4f81308846924c75d867f0
Lcandres16 <116099973+Lcandres16@users.noreply.github.com>
2024-02-05 09:23:20 -0500

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ App.tsx
@@ -0,0 +1,40 @@
+
+
+import "react-native-gesture-handler";
+
+	import React, { useEffect } from "react";
+	import { TamaguiProvider } from 'tamagui';
+	import * as SplashScreen from 'expo-splash-screen';
+	import { useFonts } from 'expo-font';
+
+	import config from './tamagui.config'
+
+	SplashScreen.preventAutoHideAsync();
+
+
+import RootStack from "./src/navigation";
+
+export default function App() {
+	
+		const [loaded] = useFonts({
+			Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+			InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+		});
+
+		useEffect(() => {
+			if (loaded) {
+				SplashScreen.hideAsync();
+			}
+		}, [loaded])
+
+		if (!loaded) {
+			return null;
+		}
+
+		return (
+			<TamaguiProvider config={config}>
+				<RootStack />
+			</TamaguiProvider>
+		);
+	
+}
